a365ae019b8a5f3f8347f787ed4cc77331ead192
sumsum16 <sharlaievaa@gmail.com>
2020-11-30 22:51:26 +0100

    data edited

--- app/models_data.py
+++ app/models_data.py
@@ -28,22 +28,22 @@ def load_models_data():
     # def __init__(self, _username, _password, _first_name, _last_name, _email, _birth_date):
 
     # 1 ADMIN, 3 DIRECTORS; 4 RECEPTIONISTS, 8 CUSTOMERS
-    admin_0 = User('Admin', 'Admin 0', 'admin@gmail.com', 'a0qwerty', UserRole.ADMIN.value)
+    admin_0 = User('Martin', 'Admin 0', 'admin@gmail.com', 'a0qwerty', UserRole.ADMIN.value)
 
     # ПОМЕНЯЛ ПАРОЛИ ЧТОБ БЫЛО УДОБНО ВВОДИТЬ. НИКАКИХ НИЖНИХ ДЕФИСОВ
-    director_0 = User('Ivan', 'Director 0', 'director0@gmail.com', 'd0qwerty', UserRole.DIRECTOR.value)
-    director_1 = User('Ivan', 'Director 1', 'director1@gmail.com', 'd1qwerty', UserRole.DIRECTOR.value)
-    director_2 = User('Ivan', 'Director 2', 'director2@gmail.com', 'd2qwerty', UserRole.DIRECTOR.value)
+    director_0 = User('Jan', 'Director 0', 'director0@gmail.com', 'd0qwerty', UserRole.DIRECTOR.value)
+    director_1 = User('Jiří', 'Director 1', 'director1@gmail.com', 'd1qwerty', UserRole.DIRECTOR.value)
+    director_2 = User('Jana', 'Director 2', 'director2@gmail.com', 'd2qwerty', UserRole.DIRECTOR.value)
 
-    customer_0 = User('Vasia', 'Customer 0', 'customer0@gmail.com', 'c0qwerty', UserRole.CUSTOMER.value)
-    customer_1 = User('Vasia', 'Customer 1', 'customer1@gmail.com', 'c1qwerty', UserRole.CUSTOMER.value)
-    customer_2 = User('Vasia', 'Customer 2', 'customer2@gmail.com', 'c2qwerty', UserRole.CUSTOMER.value)
-    customer_3 = User('Vasia', 'Customer 3', 'customer3@gmail.com', 'c3qwerty', UserRole.CUSTOMER.value)
+    customer_0 = User('Pavel', 'Customer 0', 'customer0@gmail.com', 'c0qwerty', UserRole.CUSTOMER.value)
+    customer_1 = User('Marie', 'Customer 1', 'customer1@gmail.com', 'c1qwerty', UserRole.CUSTOMER.value)
+    customer_2 = User('Martin', 'Customer 2', 'customer2@gmail.com', 'c2qwerty', UserRole.CUSTOMER.value)
+    customer_3 = User('Petr', 'Customer 3', 'customer3@gmail.com', 'c3qwerty', UserRole.CUSTOMER.value)
 
-    customer_4 = User('Vasia', 'Customer 4', 'customer4@gmail.com', 'c4qwerty', UserRole.CUSTOMER.value)
-    customer_5 = User('Vasia', 'Customer 5', 'customer5@gmail.com', 'c5qwerty', UserRole.CUSTOMER.value)
-    customer_6 = User('Vasia', 'Customer 6', 'customer6@gmail.com', 'c6qwerty', UserRole.CUSTOMER.value)
-    customer_7 = User('Vasia', 'Customer 7', 'customer7@gmail.com', 'c7qwerty', UserRole.CUSTOMER.value)
+    customer_4 = User('Hana', 'Customer 4', 'customer4@gmail.com', 'c4qwerty', UserRole.CUSTOMER.value)
+    customer_5 = User('Eva', 'Customer 5', 'customer5@gmail.com', 'c5qwerty', UserRole.CUSTOMER.value)
+    customer_6 = User('Michal', 'Customer 6', 'customer6@gmail.com', 'c6qwerty', UserRole.CUSTOMER.value)
+    customer_7 = User('Josef', 'Customer 7', 'customer7@gmail.com', 'c7qwerty', UserRole.CUSTOMER.value)
 
     db.session.add_all([
         admin_0,
@@ -52,8 +52,8 @@ def load_models_data():
         customer_4, customer_5, customer_6, customer_7,
     ])
 
-    address_0 = Address('Czech Republic', 'Brno', '602 00', 'Masarykova', '21')
-    address_1 = Address('Czech Republic', 'Praha-Vinohrady', '120 00', 'Korunni', '691')
+    address_0 = Address('Česká republika', 'Brno', '602 00', 'Masarykova', '21')
+    address_1 = Address('Česká republika', 'Praha-Vinohrady', '120 00', 'Korunni', '691')
     address_2 = Address('Česká republika', 'Brno', '602 00', 'Benesova', '605')
     address_3 = Address('Česká republika', 'Praha 6', '165 00', 'Sídlištní', '1/1167')
 
@@ -61,25 +61,36 @@ def load_models_data():
     # def __init__(self, _name, _description, _stars, _address, _owner):
 
     hotel_0 = Hotel('Jizni Morava International',
-        'Luxusni hotel v Brne pro podnikatele',
+        "Hotel se nachází v centru města, na Šilingrově náměstí. Je umístěný v prestižní "+
+        "historické budově. Hosté tu mohou zdarma využívat saunu, relaxační zónu a moderní fitness centrum."+
+        "Ve všech prostorách je zdarma dostupné Wi-Fi. Jen pár kroků od ubytování najdete celou řadu historických památek "+
+        "včetně katedrály sv. Petra a Pavla či hradu Špilberk. Jedinečně zařízené pokoje a apartmá vám zajistí "+
+        "pohodlí pro ničím nerušený spánek. Všechny mají příslušenství pro přípravu čaje a kávy, minibar, fén, "+
+        "kosmetické zrcátko, trezor, Wi-Fi, nabídku polštářů, žehličku a žehlicí prkno. Některé lze propojit a vytvořit tak velký rodinný apartmán.",
         HotelStars.FIVE.value, address_0, director_0)
 
-    receptionist_0_1 = User('JM Oleg',  'Receptionist 1', 'JMreceptionist1@gmail.com', 'JMr1qwerty', UserRole.RECEPTIONIST.value, True, hotel_0)
-    receptionist_0_2 = User('JM Petra', 'Receptionist 2', 'JMreceptionist2@gmail.com', 'JMr2qwerty', UserRole.RECEPTIONIST.value, True, hotel_0)
+    receptionist_0_1 = User('Oleg',  'JM Receptionist 1', 'JMreceptionist1@gmail.com', 'JMr1qwerty', UserRole.RECEPTIONIST.value, True, hotel_0)
+    receptionist_0_2 = User('Petra', 'JM Receptionist 2', 'JMreceptionist2@gmail.com', 'JMr2qwerty', UserRole.RECEPTIONIST.value, True, hotel_0)
 
     hotel_1 = Hotel('Praha President Hotel',
-        'Nejlepsi hotel v Praze pro prezidenty',
+        "Tento moderní business hotel stojí na břehu Vltavy v historickém centru Prahy." +
+        "Prostorné a elegantně zařízené pokoje v hotelu President mají klimatizaci, minibar, posezení, "+
+        "satelitní TV a výhled na Pražský hrad, na řeku nebo na historické centrum města." +
+        "V restauraci hotelu se každé ráno podává snídaně. Hotel President stojí v bezprostřední "+
+        "blízkosti židovské čtvrti a Pařížské ulice, 1 minutu chůze od historického centra města.",
         HotelStars.FOUR.value, address_1, director_1)
 
-    receptionist_1_1 = User('PP Martin', 'Receptionist 1', 'PPreceptionist1@gmail.com', 'PPr1qwerty', UserRole.RECEPTIONIST.value, True, hotel_1)
-    receptionist_1_2 = User('PP Jirka',  'Receptionist 2', 'PPreceptionist2@gmail.com', 'PPr2qwerty', UserRole.RECEPTIONIST.value, True, hotel_1)
+    receptionist_1_1 = User('Martin', 'PP Receptionist 1', 'PPreceptionist1@gmail.com', 'PPr1qwerty', UserRole.RECEPTIONIST.value, True, hotel_1)
+    receptionist_1_2 = User('Jirka',  'PP Receptionist 2', 'PPreceptionist2@gmail.com', 'PPr2qwerty', UserRole.RECEPTIONIST.value, True, hotel_1)
 
     hotel_2 = Hotel('Brno Grand Hotel',
-        'Kolem hotelu je vzdycky hodne opilcu, bezdomovcu a vselijakych sebrancu',
+        "Tento hotel s dlouhou 140letou tradicí na poli pohostinství stojí ve středu malebného historického centra Brna, "+
+        "v blízkosti všech hlavních památek, a jeho součástí je Garden Restaurant Le Grand připravující pokrmy české a "+
+        "mezinárodní kuchyně i lobby bar s nabídkou nápojů a lehkého občerstvení.",
         HotelStars.THREE.value, address_2, director_2)
 
-    receptionist_2_1 = User('BG Honza',     'Receptionist 1', 'BGreceptionist1@gmail.com', 'PPr1qwerty', UserRole.RECEPTIONIST.value, True, hotel_2)
-    receptionist_2_2 = User('BG Katarina',  'Receptionist 2', 'BGreceptionist2@gmail.com', 'PPr2qwerty', UserRole.RECEPTIONIST.value, True, hotel_2)
+    receptionist_2_1 = User('Honza',     'BG Receptionist 1', 'BGreceptionist1@gmail.com', 'BGr1qwerty', UserRole.RECEPTIONIST.value, True, hotel_2)
+    receptionist_2_2 = User('Katarina',  'BG Receptionist 2', 'BGreceptionist2@gmail.com', 'BGr2qwerty', UserRole.RECEPTIONIST.value, True, hotel_2)
 
     hotel_3 = Hotel('Prague Hotel Carl Inn Free Parking',
         "Prague Hotel Carl Inn Free Parking se nachází vedle autobusové zastávky "+
@@ -97,8 +108,8 @@ def load_models_data():
         "Na požádání a za příplatek lze zajistit kyvadlovou dopravu.",
         HotelStars.FOUR.value, address_3, director_0)
 
-    receptionist_3_1 = User('PH Marek',     'Receptionist 1', 'PHreceptionist1@gmail.com', 'PHr1qwerty', UserRole.RECEPTIONIST.value, True, hotel_3)
-    receptionist_3_2 = User('PH Iveta',     'Receptionist 2', 'PHreceptionist2@gmail.com', 'PHr2qwerty', UserRole.RECEPTIONIST.value, True, hotel_3)
+    receptionist_3_1 = User('Marek',     'PH Receptionist 1', 'PHreceptionist1@gmail.com', 'PHr1qwerty', UserRole.RECEPTIONIST.value, True, hotel_3)
+    receptionist_3_2 = User('Iveta',     'PH Receptionist 2', 'PHreceptionist2@gmail.com', 'PHr2qwerty', UserRole.RECEPTIONIST.value, True, hotel_3)
 
     address_0.hotel_id = hotel_0.id
     address_1.hotel_id = hotel_1.id
@@ -123,22 +134,24 @@ def load_models_data():
     # def __init__(self, _type, _price, _hotel):
 
     room_cat_0_0 = RoomCategory(RoomType.LUX.value,      2500, hotel_0,
-        "Luxusni pokoj")
+        "Elegantní pokoj s klimatizací, zdarma dostupným Wi-Fi a bezplatným vstupem do sauny a fitness centra. ")
     room_cat_0_1 = RoomCategory(RoomType.STANDARD.value, 2000, hotel_0,
-        "Standardni pokoj na miru")
+        "Tyto klimatizované pokoje jsou vybavené satelitní TV s plochou obrazovkou a minibarem.")
 
     room_cat_1_0 = RoomCategory(RoomType.LUX.value,      2000, hotel_1,
-        "Luxusni pokoj Prestiz++")
+        "Pokoj s panoramatickým výhledem na město a vstupem do Executive Lounge a wellness ")
     room_cat_1_1 = RoomCategory(RoomType.STANDARD.value, 1500, hotel_1,
-        "Pokud nejste prezident -> tak standard")
+        "Pokoj s volným vstupem do zoo")
 
     room_cat_2_0 = RoomCategory(RoomType.LUX.value,      1500, hotel_2,
-        "Dokonaly lux s holky vevnitr")
+        "Prostorná apartmá s individuálně nastavitelnou klimatizací, satelitní TV s plochou obrazovkou, minibarem, "+
+        "příslušenstvím pro přípravu čaje a kávy, trezorem a psacím stolem." +
+        "Na vyžádání a dle dostupnosti je k dispozici apartmá s balkonem. ")
     room_cat_2_1 = RoomCategory(RoomType.STANDARD.value, 1200, hotel_2,
-        "Vyhodny standard")
+        "Tyto prostorné apartma mají obývací pokoj, Wi-Fi, příslušenství pro přípravu kávy a čaje.")
 
     room_cat_3_0 = RoomCategory(RoomType.STANDARD.value, 2000, hotel_3,
-        "Prazsky standard za Prazskou cenu")
+        "Užijte si pohodlný pobyt v tomto světlém pokoji s velkými okny")
     room_cat_3_1 = RoomCategory(RoomType.BUSINESS.value, 2500, hotel_3,
         "Urcen pro podnikave lidi")
 
@@ -270,38 +283,38 @@ def load_models_data():
     reserv6 = Reservation(ReservationStatus.CHECKED_OUT.value, customer_3, payment3)
     reserv7 = Reservation(ReservationStatus.NEW.value, customer_0, payment2)
 
-    res_room = ReservationRoom(datetime.today(), datetime.today() + timedelta(days=5))
+    res_room = ReservationRoom("2020-12-01", "2020-12-06")
     res_room.room = room_2_0_1
     reserv1.reservations_rooms.append(res_room)
 
-    res_room = ReservationRoom(datetime.today(), datetime.today() + timedelta(days=5))
+    res_room = ReservationRoom("2020-12-06", "2020-12-10")
     res_room.room = room_1_0_2
     reserv1.reservations_rooms.append(res_room)
 
-    res_room = ReservationRoom(datetime.today() - timedelta(days=7), datetime.today() - timedelta(days=1))
+    res_room = ReservationRoom("2020-11-20", "2020-12-05")
     res_room.room = room_1_0_2
     reserv2.reservations_rooms.append(res_room)
 
-    res_room = ReservationRoom(datetime.today() + timedelta(days=2), datetime.today() + timedelta(days=5))
+    res_room = ReservationRoom("2020-12-9", "2020-12-15")
     res_room.room = room_1_0_0
     reserv3.reservations_rooms.append(res_room)
 
-    res_room = ReservationRoom(datetime.today() - timedelta(days=10), datetime.today() - timedelta(days=5))
+    res_room = ReservationRoom("2020-11-21", "2020-11-29")
     res_room.room = room_2_0_1
     reserv4.reservations_rooms.append(res_room)
-    res_room = ReservationRoom(datetime.today() + timedelta(days=1), datetime.today() + timedelta(days=7))
+    res_room = ReservationRoom("2020-11-30", "2020-12-11")
     res_room.room = room_2_0_2
     reserv4.reservations_rooms.append(res_room)
 
-    res_room = ReservationRoom(datetime.today() - timedelta(days=15), datetime.today() - timedelta(days=9))
+    res_room = ReservationRoom("2020-12-12", "2020-12-16")
     res_room.room = room_0_0_2
     reserv5.reservations_rooms.append(res_room)
 
-    res_room = ReservationRoom(datetime.today() - relativedelta(months=1, days=13), datetime.today() - relativedelta(months=1, days=9))
+    res_room = ReservationRoom("2020-12-21", "2020-12-27")
     res_room.room = room_0_1_1
     reserv6.reservations_rooms.append(res_room)
 
-    res_room = ReservationRoom(datetime.today() + relativedelta(days=10), datetime.today() + relativedelta(days=13))
+    res_room = ReservationRoom("2020-12-14", "2020-12-23")
     res_room.room = room_1_0_2
     reserv7.reservations_rooms.append(res_room)
 
